9069d68237474dd8faba2b7e9dbabae9d304b203
Colm O'Connor <colm.oconnor.github@gmail.com>
2016-11-24 18:14:07 +0000

    BUG : Fixed globbing when there is no subdirectory specified.

--- pathquery/__init__.py
+++ pathquery/__init__.py
@@ -10,6 +10,8 @@ class pathq(object):
     def __init__(self, path):
         self._path = path
         self._head, self._tail = split(str(self._path))
+        if self._head == '':
+            self._head = '.'
         self._but_not = []
 
     def but_not(self, paths):
@@ -23,12 +25,14 @@ class pathq(object):
         for root, dirnames, filenames_in_dir in walk(self._head):
             for filename_in_dir in filenames_in_dir:
                 full_filename = join(root, filename_in_dir)
-                if fnmatch(full_filename, self._path):
+                if fnmatch(full_filename, self._tail):
                     is_match = True
                     for but_not in self._but_not:
                         if fnmatch(full_filename, but_not._path):
                             is_match = False
                     if is_match:
+                        if full_filename.startswith("./"):
+                            full_filename = full_filename[2:]
                         matches.append(Path(full_filename))
         return matches
 
