968ab5b7476a5b9c25d87603d1d4a5b73a839392
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-01-21 23:13:22 +0000

    TOOLING : Refactored key.py a bit.

--- hitch/key.py
+++ hitch/key.py
@@ -52,28 +52,6 @@ class Engine(BaseEngine):
             linktopath = self.path.state.joinpath(linkto)
             linktopath.symlink(filepath)
 
-
-        #self.python_package = hitchpython.PythonPackage(
-            #self.given['python version']
-        #)
-        #self.python_package.build()
-
-        #self.pip = self.python_package.cmd.pip
-        #self.python = self.python_package.cmd.python
-
-        ## Install debugging packages
-        #with hitchtest.monitor([self.path.key.joinpath("debugrequirements.txt")]) as changed:
-            #if changed:
-                #run(self.pip("install", "-r", "debugrequirements.txt").in_dir(self.path.key))
-
-        ## Uninstall and reinstall
-        #with hitchtest.monitor(
-            #pathq(self.path.project.joinpath("pathquery")).ext("py")
-        #) as changed:
-            #if changed:
-                #run(self.pip("uninstall", "pathquery", "-y").ignore_errors())
-                #run(self.pip("install", ".").in_dir(self.path.project))
-        
         pylibrary = hitchbuildpy.PyLibrary(
             base_python=hitchbuildpy.PyenvBuild("3.5.0").with_build_path(self.path.share),
             module_name="pathquery",
