2a8e218b9c3d3a77fa8ee447050e0527274844de
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-04-22 17:29:09 +0100

    REFACTOR : Separated out the conceptual 'pattern' object in to a different class.

--- pathquery/query.py
+++ pathquery/query.py
@@ -3,7 +3,38 @@ from pathquery import exceptions
 from fnmatch import fnmatch
 from path import Path
 from os import walk
-import copy
+from copy import copy
+
+
+class Pattern(object):
+    def __init__(self):
+        self.is_directory = None
+        self.is_symlink = None
+        self.but_not = []
+        self.glob = None
+        self.ext = None
+        self.named = None
+
+    def match(self, filename_in_dir, full_filename):
+        is_match = True
+
+        if self.is_directory is not None:
+            if self.is_directory != isdir(full_filename):
+                is_match = False
+        if self.is_symlink is not None:
+            if self.is_symlink != islink(full_filename):
+                is_match = False
+        if self.glob is not None:
+            if not fnmatch(filename_in_dir, self.glob):
+                is_match = False
+        if self.named is not None:
+            if basename(filename_in_dir) != self.named:
+                is_match = False
+        if self.ext is not None:
+            if splitext(full_filename)[1] != ".{0}".format(self.ext):
+                is_match = False
+
+        return is_match
 
 
 class pathquery(object):
@@ -11,7 +42,7 @@ class pathquery(object):
     PathQuery represents a lazily executed search for files.
 
     Usage::
-      for filepath in pathq('yourpath/'):
+      for filepath in pathquery('yourpath/'):
           filepath.chmod("777")
     """
     def __init__(self, path):
@@ -20,55 +51,57 @@ class pathquery(object):
             raise exceptions.PathDoesNotExist(self._path)
         if not isdir(self._path):
             raise exceptions.PathIsNotDirectory(self._path)
-        self._is_directory = None
-        self._is_symlink = None
+        self._pattern = Pattern()
         self._but_not = []
-        self._glob = None
-        self._ext = None
-        self._named = None
 
     def is_dir(self):
-        new_pathq = copy.copy(self)
-        new_pathq._is_directory = True
+        new_pathq = copy(self)
+        new_pathq._pattern.is_directory = True
         return new_pathq
 
     def is_not_dir(self):
-        new_pathq = copy.copy(self)
-        new_pathq._is_directory = False
+        new_pathq = copy(self)
+        new_pathq._pattern.is_directory = False
         return new_pathq
 
     def is_symlink(self):
-        new_pathq = copy.copy(self)
-        new_pathq._is_symlink = True
+        new_pathq = copy(self)
+        new_pathq._pattern.is_symlink = True
         return new_pathq
 
     def is_not_symlink(self):
-        new_pathq = copy.copy(self)
-        new_pathq._is_symlink = False
+        new_pathq = copy(self)
+        new_pathq._pattern.is_symlink = False
         return new_pathq
 
     def glob(self, text):
-        new_pathq = copy.copy(self)
-        new_pathq._glob = text
+        new_pathq = copy(self)
+        new_pathq._pattern.glob = text
         return new_pathq
 
     def named(self, text):
-        new_pathq = copy.copy(self)
-        new_pathq._named = text
+        new_pathq = copy(self)
+        new_pathq._pattern.named = text
         return new_pathq
 
     def ext(self, extension):
         """
         Match files with an extension - e.g. 'js', 'txt'
         """
-        new_pathq = copy.copy(self)
-        new_pathq._ext = extension
+        new_pathq = copy(self)
+        new_pathq._pattern.ext = extension
         return new_pathq
 
     def __sub__(self, paths):
         assert isinstance(paths, pathquery), "{0} must be pathquery object.".format(paths)
-        new_pathq = copy.copy(self)
+        new_pathq = copy(self)
         new_pathq._but_not.append(paths)
+        return new_pathq
+
+    def __copy__(self):
+        new_pathq = pathquery(self._path)
+        new_pathq._pattern = copy(self._pattern)
+        new_pathq._but_not = copy(self._but_not)
         return new_pathq
 
     def _is_match(self, root, filename_in_dir):
@@ -77,21 +110,8 @@ class pathquery(object):
         for but_not in self._but_not:
             if but_not._path in full_filename:
                 is_match = False
-        if self._is_directory is not None:
-            if self._is_directory != isdir(full_filename):
-                is_match = False
-        if self._is_symlink is not None:
-            if self._is_symlink != islink(full_filename):
-                is_match = False
-        if self._glob is not None:
-            if not fnmatch(filename_in_dir, self._glob):
-                is_match = False
-        if self._named is not None:
-            if basename(filename_in_dir) != self._named:
-                is_match = False
-        if self._ext is not None:
-            if splitext(full_filename)[1] != ".{0}".format(self._ext):
-                is_match = False
+        if not self._pattern.match(filename_in_dir, full_filename):
+            is_match = False
         return is_match
 
     def __iter__(self):
